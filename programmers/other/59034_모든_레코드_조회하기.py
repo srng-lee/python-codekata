5be42363f7799eec16e7667aceaeae051c85d55e
srng-lee <saerahlove@gmail.com>
2026-01-22 09:25:08 +0900

    [프로그래머스] 모든 레코드 조회하기 풀이 제출
    - 문제 ID: 59034
    - 난이도: unknown
    - 제출일: 2026. 01. 22. 09:25:07
    - 작성자: 설무아

---
+++ programmers/other/59034_모든_레코드_조회하기.py
@@ -0,0 +1,9 @@
+# 모든 레코드 조회하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+# 작성자: 설무아
+# 작성일: 2026. 01. 22. 09:25:07
+
+SELECT *
+from ANIMAL_INS
+order by ANIMAL_ID
