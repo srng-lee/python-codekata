f70946a3937ca28a4f8b3de7b6f528a825b67265
srng-lee <saerahlove@gmail.com>
2026-02-26 12:01:10 +0900

    [프로그래머스] 상품 별 오프라인 매출 구하기 풀이 제출
    - 문제 ID: 131533
    - 난이도: unknown
    - 제출일: 2026. 02. 26. 12:01:10
    - 작성자: 설무아

---
+++ programmers/other/131533_상품_별_오프라인_매출_구하기.py
@@ -0,0 +1,13 @@
+# 상품 별 오프라인 매출 구하기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131533
+# 작성자: 설무아
+# 작성일: 2026. 02. 26. 12:01:10
+
+SELECT 
+    P.PRODUCT_CODE,
+    SUM(P.PRICE * S.SALES_AMOUNT) AS SALES
+FROM PRODUCT P
+JOIN OFFLINE_SALE S ON P.PRODUCT_ID = S.PRODUCT_ID
+GROUP BY P.PRODUCT_CODE
+ORDER BY SALES DESC, P.PRODUCT_CODE;
